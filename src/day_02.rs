6ed27a817cbf15920075e849fa810120d0259858
Daniele Contini <dannowonder@gmail.com>
2021-12-02 13:27:37 +0100

    feat: solve d2p2

--- src/day_02.rs
+++ src/day_02.rs
@@ -4,7 +4,7 @@ pub fn print_solution() {
     let data = get_input();
 
     println!("Part 1: {}", solve_part_1(&data));
-    println!("Part 2: {}", "");
+    println!("Part 2: {}", solve_part_2(&data));
 }
 
 fn get_input() -> Vec<(String, u32)> {
@@ -34,18 +34,40 @@ fn parse_command(x: &String) -> (String, u32) {
 }
 
 fn solve_part_1(commands: &Vec<(String, u32)>) -> u32 {
-    let mut position = (0, 0);
+    let mut position_x = 0;
+    let mut position_y = 0;
+
     for command in commands {
-        if command.0.eq(&String::from("forward")) { position.1 += command.1 }
-        else if command.0.eq(&String::from("down")) { position.0 += command.1 }
-        else if command.0.eq(&String::from("up")) { position.0 -= command.1 }
+        let direction = &command.0;
+        let lenght = command.1;
+
+        if direction.eq(&String::from("forward")) { position_x += lenght }
+        else if direction.eq(&String::from("down")) { position_y += lenght }
+        else if direction.eq(&String::from("up")) { position_y -= lenght }
     }
 
-    return position.0 * position.1;
+    return position_y * position_x;
+}
+
+fn solve_part_2(commands: &Vec<(String, u32)>) -> u32 {
+    let mut position_x = 0;
+    let mut position_y = 0;
+    let mut aim = 0;
+
+    for command in commands {
+        let direction = &command.0;
+        let lenght = command.1;
+
+        if direction.eq(&String::from("forward")) { position_x += lenght; position_y += aim * lenght }
+        else if direction.eq(&String::from("down")) { aim += lenght }
+        else if direction.eq(&String::from("up")) { aim -= lenght }
+    }
+
+    return position_y * position_x;
 }
 
 mod tests {
-    use crate::day_02::{parse_commands, solve_part_1};
+    use crate::day_02::{get_input, parse_commands, solve_part_1, solve_part_2};
 
     #[test]
     fn test_solve_part_1() {
@@ -58,6 +80,19 @@ mod tests {
             (String::from("forward"), 2),
         ];
         assert_eq!(150, solve_part_1(&commands))
+    }
+
+    #[test]
+    fn test_solve_part_2() {
+        let commands = vec![
+            (String::from("forward"), 5),
+            (String::from("down"), 5),
+            (String::from("forward"), 8),
+            (String::from("up"), 3),
+            (String::from("down"), 8),
+            (String::from("forward"), 2),
+        ];
+        assert_eq!(900, solve_part_2(&commands))
     }
 
     #[test]
@@ -82,5 +117,10 @@ mod tests {
                 (String::from("forward"), 2),
             ]
         )
+    }
+
+    #[test]
+    fn test_golden_master_solution() {
+        assert_eq!(solve_part_1(&get_input()), 1868935)
     }
 }
