273f35499f06951d956def0034275528146dfa3b
Daniele Contini <dannowonder@gmail.com>
2021-12-03 00:23:22 +0100

    merge branch refactor/day01
    Squashed commit of the following:
    
    commit a4eb7d0b00deafccd212c8b070a4cdffb3ac95eb
    Author: Daniele Contini <dannowonder@gmail.com>
    Date:   Wed Dec 1 23:41:22 2021 +0100
    
        refactor
    
    commit 465898a3d957714715a7513f2712fb545a735f00
    Author: Daniele Contini <dannowonder@gmail.com>
    Date:   Wed Dec 1 23:18:42 2021 +0100
    
        refactor
    
    commit 0cb8660a3fdaa72da573e5ab7c7fbb988ec9a0c0
    Author: Daniele Contini <dannowonder@gmail.com>
    Date:   Wed Dec 1 22:54:42 2021 +0100
    
        refactor

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,41 +1,40 @@
 use crate::input;
 
 pub fn part1() {
-    let data = input::read("data/1-1.txt")
-        .iter()
-        .map(|x| x.parse().expect("Cannot parse"))
-        .collect::<Vec<u32>>();
+    let data = get_input();
 
-    // println!("{}", solve_part1(data));
-    println!("{}", solve_part2(data));
-}
-
-fn solve_part1(depths: Vec<u32>) -> u32 {
-    let mut count = 0;
-    for i in 1..depths.len() {
-        if depths[i] > depths[i-1] {
-            count+=1
-        }
-    }
-    return count
+    println!("{}", solve_part1(&data));
 }
 
 pub fn part2() {
-    let data = input::read("data/1-1.txt")
-        .iter()
-        .map(|x| x.parse().expect("Cannot parse"))
-        .collect::<Vec<u32>>();
+    let data = get_input();
 
-    println!("{}", solve_part2(data))
+    println!("{}", solve_part2(&data))
 }
 
-fn solve_part2(depths: Vec<u32>) -> u32 {
-    let mut count = 0;
-    let mut windows = vec![];
+fn get_input() -> Vec<u32> {
+    input::read("data/1-1.txt")
+        .iter()
+        .map(|x| x.parse().expect("Cannot parse"))
+        .collect::<Vec<u32>>()
+}
 
-    for i in 0..depths.len()-2 {
-        windows.push([depths[i], depths[i+1], depths[i+2]])
-    }
+fn solve_part1(depths: &Vec<u32>) -> u32 {
+    return window_solution(depths, 1);
+}
+
+fn solve_part2(depths: &Vec<u32>) -> u32 {
+    return window_solution(depths, 3);
+}
+
+fn window_solution(depths: &Vec<u32>, window_size: u32) -> u32 {
+    let windows = (0..depths.len()-(window_size as usize-1))
+        .collect::<Vec<usize>>()
+        .iter()
+        .map(|&i| Vec::from(&depths[i..i+window_size as usize]))
+        .collect::<Vec<Vec<u32>>>();
+
+    let mut count = 0;
 
     for i in 1..windows.len() {
         let w1 = windows[i].iter().fold(0, |x, y| x + y);
@@ -51,16 +50,28 @@ fn solve_part2(depths: Vec<u32>) -> u32 {
 
 mod tests {
     use crate::day_01::{solve_part1, solve_part2};
+    use crate::input;
 
     #[test]
     fn test_solve_part1() {
         let data: Vec<u32> = Vec::from([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
-        assert_eq!(solve_part1(data), 7)
+        assert_eq!(solve_part1(&data), 7)
     }
 
     #[test]
     fn test_solve_part2() {
         let data: Vec<u32> = Vec::from([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]);
-        assert_eq!(solve_part2(data), 5)
+        assert_eq!(solve_part2(&data), 5)
+    }
+
+    #[test]
+    fn test_golden_master_solution() {
+        let data = input::read("data/1-1.txt")
+            .iter()
+            .map(|x| x.parse().expect("Cannot parse"))
+            .collect::<Vec<u32>>();
+
+        assert_eq!(solve_part1(&data), 1288);
+        assert_eq!(solve_part2(&data), 1311)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod day_01;
 
 fn main() {
     day_01::part1();
+    day_01::part2();
 }
