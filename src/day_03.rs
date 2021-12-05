f84acf73e082a0c3c4138e81804292d470f07a85
Daniele Contini <dannowonder@gmail.com>
2021-12-05 21:50:17 +0100

    faet: solve d3p2

--- src/day_03.rs
+++ src/day_03.rs
@@ -6,7 +6,7 @@ pub fn print_solution() {
 
     println!("# Day 3");
     println!("Part 1: {}", solve_part_1(&data));
-    // println!("Part 2: {}", solve_part_2(&data));
+    println!("Part 2: {}", solve_part_2(&data));
 }
 
 fn solve_part_1(report: &Vec<String>) -> i32 {
@@ -15,7 +15,7 @@ fn solve_part_1(report: &Vec<String>) -> i32 {
     for i in 0..report[0].len() {
         let digits = digits_at_position(report, i);
 
-        let most_common_value = most_common_value(report, digits);
+        let most_common_value = most_common_value(digits);
 
         gamma.push_str(most_common_value);
     }
@@ -23,6 +23,46 @@ fn solve_part_1(report: &Vec<String>) -> i32 {
     let mut epsilon = reverse_binary(&gamma);
 
     binstr_to_int(&gamma[..]) * binstr_to_int(&epsilon[..])
+}
+
+fn solve_part_2(report: &Vec<String>) -> i32 {
+    let len = report[0].len();
+
+    let co2_scrubber_rating = *(0..len)
+        .into_iter()
+        .fold(report.clone(), |list, i| {
+            if list.len() == 1 { return list }
+            let mcv = most_common_value(digits_at_position(&list, i));
+            list
+                .iter()
+                .filter(|record| &record[i..i + 1] == mcv)
+                .map(|x| String::from(x))
+                .collect::<Vec<String>>()
+        })
+        .iter()
+        .map(|x| binstr_to_int(&x[..]))
+        .collect::<Vec<i32>>()
+        .get(0)
+        .expect("Empty list");
+
+    let oxygen_generator_rating= *(0..len)
+        .into_iter()
+        .fold(report.clone(), |list, i| {
+            if list.len() == 1 { return list }
+            let mcv = less_common_value(digits_at_position(&list, i));
+            list
+                .iter()
+                .filter(|record| &record[i..i + 1] == mcv)
+                .map(|x| String::from(x))
+                .collect::<Vec<String>>()
+        })
+        .iter()
+        .map(|x| binstr_to_int(&x[..]))
+        .collect::<Vec<i32>>()
+        .get(0)
+        .expect("Empty list");
+
+    co2_scrubber_rating * oxygen_generator_rating
 }
 
 fn digits_at_position(report: &Vec<String>, i: usize) -> Vec<u32> {
@@ -40,32 +80,31 @@ fn reverse_binary(number: &String) -> String {
         .collect::<String>()
 }
 
-fn most_common_value(report: &Vec<String>, digits: Vec<u32>) -> &str {
+fn most_common_value(digits: Vec<u32>) -> &'static str {
+    criteria_common_value(digits, |sum, length| sum >= length)
+}
+
+fn less_common_value(digits: Vec<u32>) -> &'static str {
+    criteria_common_value(digits, |sum, length| sum < length)
+}
+
+fn criteria_common_value(digits: Vec<u32>, strategy: fn(f64, f64) -> bool) -> &'static str {
     let sum = digits
         .iter()
         .sum::<u32>() as f64;
 
-    let most_common_value = match sum > (report.len() / 2) as f64 {
+    match strategy(sum, (digits.len() as f64 / 2.0)) {
         true => { "1" }
         false => { "0" }
-    };
-
-    most_common_value
+    }
 }
-
-// fn solve_part_2(report: &Vec<String>) -> i32 {
-//     let oxygen_generator_rating = 0;
-//     let CO2_scrubber_rating = 0;
-//
-//     0
-// }
 
 fn binstr_to_int(bin: &str) -> i32 {
     i32::from_str_radix(bin, 2).expect("Not a binary number!")
 }
 
 mod tests {
-    use crate::day_03::{binstr_to_int, reverse_binary, solve_part_1};
+    use crate::day_03::{binstr_to_int, less_common_value, most_common_value, reverse_binary, solve_part_1, solve_part_2};
     use crate::input;
 
     #[test]
@@ -100,29 +139,48 @@ mod tests {
         assert_eq!(solve_part_1(&report), 198)
     }
 
-    // #[test]
-    // fn test_solve_part_2() {
-    //     let report = vec![
-    //         String::from("00100"),
-    //         String::from("11110"),
-    //         String::from("10110"),
-    //         String::from("10111"),
-    //         String::from("10101"),
-    //         String::from("01111"),
-    //         String::from("00111"),
-    //         String::from("11100"),
-    //         String::from("10000"),
-    //         String::from("11001"),
-    //         String::from("00010"),
-    //         String::from("01010"),
-    //     ];
-    //
-    //     assert_eq!(solve_part_2(&report), 230)
-    // }
+    #[test]
+    fn test_solve_part_2() {
+        let report = vec![
+            String::from("00100"),
+            String::from("11110"),
+            String::from("10110"),
+            String::from("10111"),
+            String::from("10101"),
+            String::from("01111"),
+            String::from("00111"),
+            String::from("11100"),
+            String::from("10000"),
+            String::from("11001"),
+            String::from("00010"),
+            String::from("01010"),
+        ];
+
+        assert_eq!(solve_part_2(&report), 230)
+    }
 
     #[test]
     fn test_golden_master() {
         let data = input::read("data/03.txt");
         assert_eq!(solve_part_1(&data), 2003336);
+        assert_eq!(solve_part_2(&data), 1877139);
+    }
+
+    #[test]
+    fn test_most_common_value() {
+        assert_eq!(most_common_value(vec![0, 1]), "1");
+        assert_eq!(most_common_value(vec![0, 1, 1]), "1");
+        assert_eq!(most_common_value(vec![0, 0, 1, 1]), "1");
+        assert_eq!(most_common_value(vec![0, 0, 0, 1, 1]), "0");
+        assert_eq!(most_common_value(vec![0, 0, 0, 0, 1, 1]), "0");
+    }
+
+    #[test]
+    fn test_less_common_value() {
+        assert_eq!(less_common_value(vec![0, 1]), "0");
+        assert_eq!(less_common_value(vec![0, 1, 1]), "0");
+        assert_eq!(less_common_value(vec![0, 0, 1, 1]), "0");
+        assert_eq!(less_common_value(vec![0, 0, 0, 1, 1]), "1");
+        assert_eq!(less_common_value(vec![0, 0, 0, 0, 1, 1]), "1");
     }
 }
