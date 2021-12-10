561776d798b6423967462b058440ace8727e46db
Daniele Contini <dannowonder@gmail.com>
2021-12-11 00:44:21 +0100

    feat: solve d7p2

--- src/day_07.rs
+++ src/day_07.rs
@@ -1,4 +1,4 @@
-use std::cmp::min;
+use std::cmp::{max, min};
 use crate::input;
 
 pub fn print_solution() {
@@ -24,21 +24,37 @@ fn solve_part_1(data: &Vec<i64>) -> i64 {
     let &max = data.iter().max().expect("Empty");
 
     (min..=max)
-        .map(|i| fuel_cost(&data, i))
+        .map(|i| fuel_cost_constant(&data, i))
         .min()
         .expect("Empty")
 }
 
 fn solve_part_2(data: &Vec<i64>) -> i64 {
-    0
+    let &min = data.iter().min().expect("Empty");
+    let &max = data.iter().max().expect("Empty");
+
+    (min..=max)
+        .map(|i| fuel_cost_linear(&data, i))
+        .min()
+        .expect("Empty")
 }
 
-fn fuel_cost(data: &Vec<i64>, position: i64) -> i64 {
+fn fuel_cost_constant(data: &Vec<i64>, position: i64) -> i64 {
     data.iter().fold(0, |c, &x| c + (position-x).abs())
 }
 
+fn fuel_cost_linear(data: &Vec<i64>, position_end: i64) -> i64 {
+    data
+        .iter()
+        .fold(0, |carry_cost, &position_start| {
+            let range = (min(position_start, position_end)..max(position_start, position_end));
+            let cost_move_grab = range.fold((1, 0), |a, _y| (a.0 + 1, a.1 + a.0)).1;
+            carry_cost + cost_move_grab
+        })
+}
+
 mod tests {
-    use crate::day_07::{fuel_cost, parse_input, solve_part_1, solve_part_2};
+    use crate::day_07::{fuel_cost_constant, fuel_cost_linear, parse_input, solve_part_1, solve_part_2};
     use crate::input;
 
     fn get_input() -> Vec<i64> {
@@ -46,12 +62,19 @@ mod tests {
     }
 
     #[test]
-    fn test_fuel_cost() {
+    fn test_fuel_cost_constant() {
         let input = get_input();
-        assert_eq!(fuel_cost(&input, 2), 37);
-        assert_eq!(fuel_cost(&input, 1), 41);
-        assert_eq!(fuel_cost(&input, 3), 39);
-        assert_eq!(fuel_cost(&input, 10), 71);
+        assert_eq!(fuel_cost_constant(&input, 2), 37);
+        assert_eq!(fuel_cost_constant(&input, 1), 41);
+        assert_eq!(fuel_cost_constant(&input, 3), 39);
+        assert_eq!(fuel_cost_constant(&input, 10), 71);
+    }
+
+    #[test]
+    fn test_fuel_cost_linear() {
+        let input = get_input();
+        assert_eq!(fuel_cost_linear(&input, 5), 168);
+        assert_eq!(fuel_cost_linear(&input, 2), 206);
     }
 
     #[test]
@@ -63,7 +86,7 @@ mod tests {
     #[test]
     fn test_solve_part_2() {
         let input = get_input();
-        assert_eq!(solve_part_2(&input), 1);
+        assert_eq!(solve_part_2(&input), 168);
     }
 
     #[test]
@@ -71,6 +94,6 @@ mod tests {
         let input = input::read("data/07.txt");
         let data = parse_input(&input);
         assert_eq!(solve_part_1(&data), 344535);
-        // assert_eq!(solve_part_2(&data), 1);
+        assert_eq!(solve_part_2(&data), 95581659);
     }
 }
